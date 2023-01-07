ae236ab31fbbb829c99a082ff29cb581079f3a77
안호진 <asdv94@naver.com>
2023-01-07 21:13:38 +0900

    feat(cosntants): API, 기타 상수 추가

---
+++ src/constants/index.ts
@@ -0,0 +1,9 @@
+export const API_ENDPOINT = 'http://localhost:8080/';
+
+export const API_URLS = {
+  login: 'users/login',
+  signUp: 'users/create',
+  todos: 'todos',
+};
+
+export const TOKEN = 'token';
